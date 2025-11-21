56463517c8098e92fa626a11beb4aa89ec5476b0
Luke Griffin <griffinluke321@gmail.com>
2025-11-21 17:31:14 +0000

    updated names

--- js/config.js
+++ js/config.js
@@ -4,13 +4,13 @@ export const MODELS = [
     models: [
       {
         id: "default",
-        label: "Swervy",
+        label: "Swervy - TRSYP 2.0 - Tunisia 2025",
         path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/Swervy.glb",
         default: true
       },
       {
         id: "alt1",
-        label: "ROBOI",
+        label: "ROBOI - IEEE REGION 8 - Malta 2024",
         path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/ROBOI.glb"
       }
     ]
