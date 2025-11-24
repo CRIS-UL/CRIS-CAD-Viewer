94a015d231c1b466b08ec2ddb6a91dca2f2c2025
Luke Griffin <griffinluke321@gmail.com>
2025-11-24 09:55:47 +0000

    added Panther model

--- js/config.js
+++ js/config.js
@@ -4,14 +4,19 @@ export const MODELS = [
     models: [
       {
         id: "default",
-        label: "Swervy - TRSYP 2.0 - Tunisia 2025",
-        path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/Swervy.glb",
+        label: "Panther",
+        path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/Panther.glb",
         default: true
       },
       {
         id: "alt1",
         label: "ROBOI - IEEE REGION 8 - Malta 2024",
         path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/ROBOI.glb"
+      },
+      {
+        id: "alt2",
+        label: "Swervy - TRSYP 2.0 - Tunisia 2025",
+        path: "https://pub-eee3ac1ff5434582868c7ea0c6208333.r2.dev/models/Swervy.glb"
       }
     ]
   }
